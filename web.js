6a83bade5ade96b9e9f82dd8eed57adf9713531a
Crowdopolis <todd@Crowdopolis.com>
2013-07-04 03:01:06 -0700

    From Index

--- web.js
+++ web.js
@@ -1,4 +1,7 @@
-var express = require('express');
+var express = require('express'),
+    fs      = require('fs');
+
+/*var index = fs.readFileSync('index.html');
 
 var app = express.createServer(express.logger());
 
@@ -9,4 +12,18 @@ app.get('/', function(request, response) {
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-});
+});*/
+
+var http = require('http');
+var fs = require('fs');
+var sys = require('sys');
+
+var port = process.env.PORT || 5000;
+
+http.createServer(function(req, res){
+    fs.readFile('test.html',function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        res.write(data);
+        res.end();
+    });
+}).listen(port);
